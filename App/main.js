8b74d1f8dfba86bda795de1fb7a6c551e824653d
Kyle Lobo <kylelobo11898@gmail.com>
2019-03-23 04:44:20 +0530

    Added home and about pages

--- App/main.js
+++ App/main.js
@@ -2,10 +2,10 @@ const electron  = require('electron');
 const path      = require('path');
 const url       = require('url');
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, clipboard} = electron;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({ backgroundColor: '#0099cc' });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
@@ -21,15 +21,90 @@ app.on('ready', () => {
 
 const mainMenuTemplate =  [
     {
-        label: 'Menu',
+        label: 'File',
         submenu: [
             {
                 label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click() {
                     app.quit();
                 }
             }
         ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                role: 'undo'
+            },
+            {
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'cut'
+            },
+            {
+                role: 'copy'
+            },
+            {
+                role: 'paste'
+            }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                role: 'toggledevtools'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'resetzoom'
+            },
+            {
+                role: 'zoomin'
+            },
+            {
+                role: 'zoomout'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'togglefullscreen'
+            }
+        ]
+    },
+    {
+        label: 'Window',
+        submenu: [
+            {
+                role: 'minimize'
+            },
+            {
+                role: 'close'
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: [
+            {
+                label: 'Learn More'
+            }
+        ]
     }
 ];
+
+if (process.platform === 'darwin') {
+    mainMenuTemplate.unshift({});
+}
